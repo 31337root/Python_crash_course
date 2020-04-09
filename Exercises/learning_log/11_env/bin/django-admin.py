d6b650b076b003c0e424f22d3a30a68b56094510
Omar Martinez <omarma@outlook.es>
2020-04-08 22:33:39 -0500

    Adding learning_log project

---
+++ Exercises/learning_log/11_env/bin/django-admin.py
@@ -0,0 +1,5 @@
+#!/Users/mac/Desktop/Git/31337root/Python_crash_course/Exercises/learning_log/11_env/bin/python3
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
