010f73a7ac0555bb7d37219ff1ac43b0147ea37d
Omar Martinez <omarma@outlook.es>
2020-04-14 13:00:41 -0500

    Deleting learling_log

--- Exercises/learning_log/11_env/bin/django-admin.py
+++
@@ -1,5 +0,0 @@
-#!/Users/mac/Desktop/Git/31337root/Python_crash_course/Exercises/learning_log/11_env/bin/python3
-from django.core import management
-
-if __name__ == "__main__":
-    management.execute_from_command_line()
